28967f7335e23c632f7968c8461167622fbb2297
Chris Berkhout <chrisberkhout@gmail.com>
2012-05-27 12:13:21 +1000

    can't do any_instance assertion.

--- spec/mysql_preview_spec.rb
+++ spec/mysql_preview_spec.rb
@@ -20,5 +20,12 @@ end
 
 describe Sequel::MySQLPreview::Database do
 
+  it "should work" do
+    pending "SystemStackError: stack level too deep"
+    query = "SELECT * FROM atable;"
+    Sequel::MySQL::Database.any_instance.should_receive(:execute).with(query, {})
+    subject.execute(query)
+  end
+
 end
 
